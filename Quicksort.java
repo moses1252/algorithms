ae4004e2e024b095cd5f09b3c0994a201734e875
Anthony Viramontes <47967342+moses1252@users.noreply.github.com>
2022-11-21 18:38:49 -0800

    added notes

--- Quicksort.java
+++ Quicksort.java
@@ -6,7 +6,8 @@ public class Quicksort {
 
 	public static void main(String[] args) {
 		Random rand = new Random();
-		int [] numbers = new int[1000000];
+		int [] numbers = new int[10000];
+		//int [] numbers = new int[10];
 		
 		for (int i = 0; i < numbers.length; i++) {
 			numbers[i] = rand.nextInt(10000);
@@ -15,31 +16,33 @@ public class Quicksort {
 		System.out.println("Before:");
 		//printArray(numbers);
 		
-		quicksort(numbers);
+		quicksort(numbers, 0, numbers.length - 1);
 		
 		System.out.println("\nAfter:");
 		//printArray(numbers);
 
 	}
 	
-	//overload method
-	private static void quicksort(int[]array) {
-		quicksort(array, 0, array.length - 1);
-	}
-	
 	private static void quicksort(int[] array, int lowIndex, int highIndex) {
 		
+		//if left pointer is greater then right pointer, return 
+		//dealing with subarry of one element, so just return it
 		if (lowIndex >= highIndex) {
 			return;
 		}
 		
 		//choose a pivot at random
-		int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex; 
+		int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
 		int pivot = array[pivotIndex];
-		swap(array, pivotIndex, highIndex); //put pivot at end of index
+		
+		//swap pivot to last index in array
+		swap(array, pivotIndex, highIndex);
 		
 		int leftPointer = partition(array, lowIndex, highIndex, pivot);
 		
+		
+		//now we recursively quick sort the partition to the left of the pivot
+		//and recursively quick sort the partition to the right of the pivot
 		quicksort(array, lowIndex, leftPointer - 1);
 		quicksort(array, leftPointer + 1, highIndex);
 	}
@@ -48,27 +51,34 @@ public class Quicksort {
 		int leftPointer = lowIndex;
 		int rightPointer = highIndex;
 		
-		
 		while (leftPointer < rightPointer) {
+			//increment leftPointer until it finds a value bigger than pivot or until it reaches right Pointer
 			while (array[leftPointer] <= pivot && leftPointer < rightPointer) {
 				leftPointer++;
 			}
 			
+			//decrement right pointer until it finds a value smaller than the pivot or until it reaches leftPoitner
 			while (array[rightPointer] >= pivot && leftPointer < rightPointer) {
 				rightPointer--;
 			}
 			
+			//swap
 			swap(array, leftPointer, rightPointer);
 		}
 		
+		//once our left and right pointer meet
+		//we want to swap our pivot at that index
+		//after this the partitioning step is done
+		//the numbers that are lower than the pivot are to the left of it
+		//the numbers that are hight than the pivot are to the right of it
 		swap(array, leftPointer, highIndex);
 		return leftPointer;
 	}
 	
-	private static void swap(int[] arr, int index1, int index2) {
-		int temp = arr[index1];
-		arr[index1] = arr[index2];
-		arr[index2] = temp;
+	private static void swap(int[] array, int leftIndex, int rightIndex) {
+		int temp = array[leftIndex];
+		array[leftIndex] = array[rightIndex];
+		array[rightIndex] = temp;
 	}
 	
 	private static void printArray(int[] numbers) {
