591614c1dd51d07b43eeeecdcbad22f63ba371cf
Anthony Viramontes <47967342+moses1252@users.noreply.github.com>
2022-11-28 20:48:06 -0800

    added notes to insertion sort

--- Insertionsort.java
+++ Insertionsort.java
@@ -2,6 +2,16 @@ package algorithms;
 
 import java.util.Random;
 
+//time complexity
+//best: O(n)
+//average: O(n^2) 
+//worst: O(n^2)
+
+//space complexity
+//worst: (1)
+
+
+
 public class Insertionsort {
 	public static void main(String[] args) {
 		Random rand = new Random();
@@ -25,8 +35,13 @@ public class Insertionsort {
 			
 			int currentValue = arr[i];
 			
+			//set j to look at value one before arr[i]
 			int j = i - 1;
+			
+			//stop while loop once we hit beginning of our array
+			//and keep going until value is arr[j] is greater then arr[i]
 			while (j >= 0 && arr[j] > currentValue) {
+				//shift value to the right
 				arr[j + 1] = arr[j];
 				j--;
 			}
